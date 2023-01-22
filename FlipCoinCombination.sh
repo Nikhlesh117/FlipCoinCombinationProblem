aca2fb0cabf2c4f10010fc41bbed10c0c8066d3a
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-22 10:32:06 +0530

    Add: Percentage of the Doublet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -2,21 +2,32 @@ echo "Welcome to Flip Coin Combination Program"
 
 declare -A dict
 
-dict[Head]=0
-dict[Tail]=0
-isHead=1
+dict[HH]=0
+dict[HT]=0
+dict[TH]=0
+dict[TT]=0
 
 for (( i=0; i<10; i++ ))
 do
-	randomCheck=$((RANDOM%2))
-
-	if [ $randomCheck -eq $isHead ]
-	then
-		((dict[Head]++))
-	else
-		((dict[Tail]++))
-	fi
+	randomCheck1=$((RANDOM%2))
+	randomCheck2=$((RANDOM%2))
+	case $randomCheck1$randomCheck2 in
+		11)
+			((dict[HH]++))
+		;;
+		10)
+			((dict[HT]++))
+		;;
+		01)
+			((dict[TH]++))
+		;;
+		00)
+			((dict[TT]++))
+		;;
+	esac
 done
 
-echo "Heads percentage: $((${dict[Head]}*100/10)) percent"
-echo "Tails percentage: $((${dict[Tail]}*100/10)) percent"
+echo "Double heads percentage: $((${dict[HH]}*100/10)) percent"
+echo "Single head and tail percentage: $((${dict[HT]}*100/10)) percent"
+echo "Single tail and head percentage: $((${dict[TH]}*100/10)) percent"
+echo "Double Tails percentage: $((${dict[TT]}*100/10)) percent"
