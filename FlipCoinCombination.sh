99782e57946630f09d68cb8995346eff0ced2dc9
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-22 10:39:39 +0530

    Add: Percentage of the Triplet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -2,32 +2,54 @@ echo "Welcome to Flip Coin Combination Program"
 
 declare -A dict
 
-dict[HH]=0
-dict[HT]=0
-dict[TH]=0
-dict[TT]=0
+dict[HHH]=0
+dict[HHT]=0
+dict[HTH]=0
+dict[THH]=0
+dict[HTT]=0
+dict[THT]=0
+dict[TTH]=0
+dict[TTT]=0
+
 
 for (( i=0; i<10; i++ ))
 do
 	randomCheck1=$((RANDOM%2))
 	randomCheck2=$((RANDOM%2))
-	case $randomCheck1$randomCheck2 in
-		11)
-			((dict[HH]++))
+	randomCheck3=$((RANDOM%2))
+	case $randomCheck1$randomCheck2$randomCheck3 in
+		111)
+			((dict[HHH]++))
 		;;
-		10)
-			((dict[HT]++))
+		110)
+			((dict[HHT]++))
 		;;
-		01)
-			((dict[TH]++))
+		101)
+			((dict[HTH]++))
 		;;
-		00)
-			((dict[TT]++))
-		;;
+		011)
+			((dict[THH]++))
+       		;;
+		100)
+                        ((dict[HTT]++))
+                ;;
+                010)
+                        ((dict[THT]++))
+                ;;
+                001)
+                        ((dict[TTH]++))
+                ;;
+                000)
+                        ((dict[TTT]++))
+
 	esac
 done
 
-echo "Double heads percentage: $((${dict[HH]}*100/10)) percent"
-echo "Single head and tail percentage: $((${dict[HT]}*100/10)) percent"
-echo "Single tail and head percentage: $((${dict[TH]}*100/10)) percent"
-echo "Double Tails percentage: $((${dict[TT]}*100/10)) percent"
+echo "Triplet percentage of HHH: $((${dict[HHH]}*100/10)) percent"
+echo "Triplet percentage of HHT: $((${dict[HHT]}*100/10)) percent"
+echo "Triplet percentage of HTH: $((${dict[HTH]}*100/10)) percent"
+echo "Triplet percentage of THH: $((${dict[THH]}*100/10)) percent"
+echo "Triplet percentage of HTT: $((${dict[HTT]}*100/10)) percent"
+echo "Triplet percentage of THT: $((${dict[THT]}*100/10)) percent"
+echo "Triplet percentage of TTH: $((${dict[TTH]}*100/10)) percent"
+echo "Triplet percentage of TTT: $((${dict[TTT]}*100/10)) percent"
