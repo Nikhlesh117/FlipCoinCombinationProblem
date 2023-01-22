4bb1add139db0dde68db3f7710a9f1c44d7682a4
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-22 10:59:57 +0530

    Add: Combine Singlet, Doublet and Triplet combination and show the winning combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,5 +1,5 @@
-echo "Welcome to Flip Coin Combination Program"
-
+function triplet
+{
 declare -A dict
 
 dict[HHH]=0
@@ -53,3 +53,90 @@ echo "Triplet percentage of HTT: $((${dict[HTT]}*100/10)) percent"
 echo "Triplet percentage of THT: $((${dict[THT]}*100/10)) percent"
 echo "Triplet percentage of TTH: $((${dict[TTH]}*100/10)) percent"
 echo "Triplet percentage of TTT: $((${dict[TTT]}*100/10)) percent"
+}
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function doublet
+{
+declare -A dict
+
+dict[HH]=0
+dict[HT]=0
+dict[TH]=0
+dict[TT]=0
+
+for (( i=0; i<10; i++ ))
+do
+	randomCheck1=$((RANDOM%2))
+	randomCheck2=$((RANDOM%2))
+	case $randomCheck1$randomCheck2 in
+		11)
+			((dict[HH]++))
+		;;
+		10)
+			((dict[HT]++))
+		;;
+		01)
+			((dict[TH]++))
+		;;
+		00)
+			((dict[TT]++))
+		;;
+	esac
+done
+
+echo "Double heads percentage: $((${dict[HH]}*100/10)) percent"
+echo "Single head and tail percentage: $((${dict[HT]}*100/10)) percent"
+echo "Single tail and head percentage: $((${dict[TH]}*100/10)) percent"
+echo "Double Tails percentage: $((${dict[TT]}*100/10)) percent"
+}
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+function singlet
+{
+declare -A dict
+
+dict[Head]=0
+dict[Tail]=0
+isHead=1
+
+for (( i=0; i<10; i++ ))
+do
+	randomCheck=$((RANDOM%2))
+
+	if [ $randomCheck -eq $isHead ]
+	then
+		((dict[Head]++))
+	else
+		((dict[Tail]++))
+	fi
+done
+
+echo "Heads percentage: $((${dict[Head]}*100/10)) percent"
+echo "Tails percentage: $((${dict[Tail]}*100/10)) percent"
+}
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+echo "Welcome to flip coin simulation"
+echo "Enter 1 for singlet"
+echo "Enter 2 for dublet"
+echo "Enter 3 for triplet"
+read number
+case $number in
+	1)
+		singlet
+		;;
+
+	2)
+		doublet
+		;;
+
+	3)
+		triplet
+		;;
+
+	*)
+		echo "Invalid input"
+ esac
