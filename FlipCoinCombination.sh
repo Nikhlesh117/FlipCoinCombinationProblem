ceec09a6fe61e832d7c663b7c5b2ec397c7467b3
“Nikhlesh117” <nikhlesh.rao@gmail.com>
2023-01-22 10:20:04 +0530

    Add: Percentage of the Singlet Combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,22 @@
 echo "Welcome to Flip Coin Combination Program"
 
-Head=1
-randomCheck=$((RANDOM%2))
+declare -A dict
 
-if [ $randomCheck -eq $Head ]
-then
-        echo "Heads"
-else
-        echo "Tails"
-fi
+dict[Head]=0
+dict[Tail]=0
+isHead=1
+
+for (( i=0; i<10; i++ ))
+do
+	randomCheck=$((RANDOM%2))
+
+	if [ $randomCheck -eq $isHead ]
+	then
+		((dict[Head]++))
+	else
+		((dict[Tail]++))
+	fi
+done
+
+echo "Heads percentage: $((${dict[Head]}*100/10)) percent"
+echo "Tails percentage: $((${dict[Tail]}*100/10)) percent"
